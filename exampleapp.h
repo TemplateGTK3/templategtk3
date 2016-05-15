22521bc3405773e3355475a1501257e3c1e3013a
TemplateGTK3 <templategtk3@gmail.com>
2016-05-15 23:36:47 +0200

    update files

--- exampleapp.h
+++ exampleapp.h
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) Laurent LAMARD 2016
+ *
+ * gepub is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * bijiben is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef __EXAMPLEAPP_H
 #define __EXAMPLEAPP_H
 
--- exampleappwin.c
+++ exampleappwin.c
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) Laurent LAMARD 2016
+ *
+ * gepub is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * bijiben is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <gtk/gtk.h>
 
 #include "exampleapp.h"
@@ -44,34 +61,5 @@ ExampleAppWindow * example_app_window_new (ExampleApp *app)
 void example_app_window_open (ExampleAppWindow *win,
                          GFile            *file)
 {
-    ExampleAppWindowPrivate *priv;
-    gchar *basename;
-    GtkWidget *scrolled, *view;
-    gchar *contents;
-    gsize length;
 
-    priv = example_app_window_get_instance_private (win);
-    basename = g_file_get_basename (file);
-
-    scrolled = gtk_scrolled_window_new (NULL, NULL);
-    gtk_widget_show (scrolled);
-    gtk_widget_set_hexpand (scrolled, TRUE);
-    gtk_widget_set_vexpand (scrolled, TRUE);
-    view = gtk_text_view_new ();
-    gtk_text_view_set_editable (GTK_TEXT_VIEW (view), FALSE);
-    gtk_text_view_set_cursor_visible (GTK_TEXT_VIEW (view), FALSE);
-    gtk_widget_show (view);
-    gtk_container_add (GTK_CONTAINER (scrolled), view);
-    gtk_stack_add_titled (GTK_STACK (priv->stack), scrolled, basename, basename);
-
-    if (g_file_load_contents (file, NULL, &contents, &length, NULL, NULL))
-    {
-        GtkTextBuffer *buffer;
-
-        buffer = gtk_text_view_get_buffer (GTK_TEXT_VIEW (view));
-        gtk_text_buffer_set_text (buffer, contents, length);
-        g_free (contents);
-    }
-
-    g_free (basename);
 }
